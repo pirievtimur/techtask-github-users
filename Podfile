46ce2e83286d2530a38a3c2b3befdae2bfadbfd9
Timur Piriev <pirievtimur1@gmail.com>
2018-09-17 11:37:14 +0300

    Create project Add cocoapods

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '10.0'
+use_frameworks!
+inhibit_all_warnings!
+
+target 'github-users' do
+  pod 'RxSwift', '~> 4.0'
+  pod 'RxCocoa', '~> 4.0'
+  pod 'ObjectMapper', '~> 3.3'
+  pod 'SnapKit', '~> 4.0.0'
+end
---
+++ github-users/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  github-users
+//
+//  Created by Timur Piriev on 9/17/18.
+//  Copyright © 2018 Timur Piriev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
